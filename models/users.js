4b0ad471ade4e4823ee89f3b8a653d28a911b58e
Lucas <lucas.bordini@hotmail.com>
2018-05-19 17:35:11 -0300

    crypt

--- models/users.js
+++ models/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataType) => { 
 
     const Users = sequelize.define('Users', { 
@@ -35,6 +37,15 @@ module.exports = (sequelize, DataType) => {
         Users.hasMany(models.Tasks, { 
             onDelete: 'CASCADE'
         });
+    };
+
+    Users.hook('beforeCreate',  user => { 
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+    });
+
+    Users.isPassword = (encodedPassword, password) => { 
+        return bcrypt.compareSync(password, encodedPassword);
     };
 
     return Users;
