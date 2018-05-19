56fb69c47abe73f3ec444f8949a7dc9f6d713070
Lucas <lucas.bordini@hotmail.com>
2018-05-19 15:35:28 -0300

    First API Call

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,5 +1,12 @@
+const bodyParser = require('body-parser');
+
 module.exports = app => {
 
     app.set('port', 3000);
+    app.use(bodyParser.json());
+    app.use((req, res, next) => { 
+        delete req.body.id;
+        next();
+    });
 
 }
